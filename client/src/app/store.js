a5757023e0b389617d90e72712696a86c24fcc8d
MohamedMeksi <miksimohamed@gmail.com>
2024-04-29 10:52:36 +0100

    gsggshg

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,14 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import adherentSlice from './adherentSlice';
 import categorySlice from './categorySlice';
-
+import subcategorySlice from './subcategorySlice';
 
 
 export const store = configureStore({
     reducer: {
         user : userReducer,
         adherent : adherentSlice,
-        category : categorySlice
+        category : categorySlice,
+        subcategory : subcategorySlice
     },
 });
 
